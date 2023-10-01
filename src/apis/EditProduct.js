9e8b8aafa97cde61a337f5a6e0f4861e9986fa35
morasa zadbagher sighalani <100466704+MORASAzb@users.noreply.github.com>
2023-10-01 13:24:22 +0330

    Update EditProduct.js

--- src/apis/EditProduct.js
+++ src/apis/EditProduct.js
@@ -2,6 +2,7 @@
 export default function EditProductItem(data){
     const res = fetch(`http://localhost:3000/products/${data._id}/`, {
         method: 'PATCH',
+        body: JSON.stringify(data)
         headers: {
             "Content-Type": "application/json",
             "Accept": "application/json"
